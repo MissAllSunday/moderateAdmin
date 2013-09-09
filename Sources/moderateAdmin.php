24f447e1b530755b2d097b4a6975ccc7162eb893
Suki <suki@missallsunday.com>
2013-09-09 14:58:14 -0500

    Sources/moderateAdmin.php helper sources file
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Sources/moderateAdmin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * @package moderateAdmin mod
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013, Jessica González
+ * @license http://www.mozilla.org/MPL/ MPL 2.0
+ */
+
+if (!defined('SMF'))
+	die('No direct access...');
